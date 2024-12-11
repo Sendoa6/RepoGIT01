1031ce69b644cbd0decc214b74aa10e0bf0fc003
Sendoa6 <sendoa.perez06@somo.eus>
2024-12-11 09:51:54 +0100

    PrimerComit

---
+++ src/clasesDatos/Empleado.java
@@ -0,0 +1,11 @@
+package clasesDatos;
+
+public class Empleado {
+	String DNI;
+	String Nombre;
+	String Apellidos;
+	int edad;
+	boolean clubSocial; // Indica si el empleado esta en el club social de la empresa
+	float sueldo;
+
+}
